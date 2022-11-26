0e2d74cffdea751b1cbae47b218321773f0413a0
meohyeon <theogus2@naver.com>
2022-11-26 18:36:17 +0900

    동빈나  n이 1이될떄까지

---
+++ youtube02.py
@@ -0,0 +1,20 @@
+# 동빈나 이코테 n이 1이 될떄까지 
+# n%k==0 k로 나누고 아니면 1을 뺀다 
+print("정수 2개 입력")
+n, k = map(int, input().split())
+
+countA=0
+
+while True :
+    if n > k:
+        a = (n//k)*k 
+        countA += (n-a)
+        n = a // k
+        countA+=1
+    else:
+        n-=1
+        countA+=1   
+    if n <= 1:
+        break
+    
+print("연산횟수 :"+str(countA))
