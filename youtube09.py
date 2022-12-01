53f0d525f5fc8b4d9eb2b7f2909216de8d7adaa5
meohyeon <theogus2@naver.com>
2022-12-01 18:52:07 +0900

    동빈나 이코테 스택 과 큐 구현

---
+++ youtube09.py
@@ -0,0 +1,26 @@
+# 동빈나 이코테 3. DFS & BFS
+
+# 스택 구현 
+stack = []
+
+stack.append(5)
+stack.append(10)
+stack.append(8)
+stack.pop()
+
+print(stack[::-1]) #최상단 원소 출력 
+
+# 큐 구현 
+from collections import deque
+
+queue = deque()
+
+queue.append(5)
+queue.append(10)
+queue.append(8)
+queue.popleft()
+print(queue)
+
+
+    
+
