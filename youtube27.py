a698d97196364e19692fd73a305e60058c8ca53a
meohyeon <theogus2@naver.com>
2022-12-06 19:37:39 +0900

    동빈나 이코테 두 포인터

--- youtube27.py
+++ youtube27.py
@@ -1,21 +1,18 @@
+# 동빈나 이코테 투포인터 
 n = int(input())
-list = list(map(int, input().split()))
-start = 0
-end = 0
-count = 0 
-for i in range(n*n):
-    print(list[start:end+1])
-    if sum(list[start:end+1]) == n:
-        count += 1
 
-    if sum(list[start:end+1]) < n:
-        if end < n:
-            end += 1
-    else:
-        if start < n:
-            start += 1
-    
-    if start == 5 and end == 5:
-        break
+list = list(map(int,input().split()))
+
+end = 0
+count = 0
+total = 0
+
+for start in range(len(list)):
+    while total < n or end < n:
+        total += list[end]
+        end += 1
+    if total == n:
+        count += 1
+    total -= list[start]
 
 print(count)
